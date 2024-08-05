03e0a712d53fa051cc253eeedca90e4bf773f65c
Gabriel Ganderup <146817067+w1setown@users.noreply.github.com>
2024-08-05 13:16:36 +0200

    Add files via upload

---
+++ Learning/Basic - Assignments/Celsius2Fahrenheit.cs
@@ -0,0 +1,21 @@
+﻿namespace Basic___Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Celsius to Fahrenheit code has been removed.
+
+            // Uncomment the following line if you want to use the AddAndMultiply method
+            Console.WriteLine(AddAndMultiply(2, 4, 5));
+        }
+
+        // | Add two numbers
+        static int AddAndMultiply(int a, int b, int c)
+        {
+            int output = 0;
+            output = (a + b) * c;
+            return output;
+        }
+    }
+}
