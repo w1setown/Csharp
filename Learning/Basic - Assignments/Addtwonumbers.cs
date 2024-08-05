ec367d08a16e7a201faa97403aab8d0f1b70b30f
Gabriel Ganderup <146817067+w1setown@users.noreply.github.com>
2024-08-05 13:18:06 +0200

    Update Addtwonumbers.cs

--- Learning/Basic - Assignments/Addtwonumbers.cs
+++ Learning/Basic - Assignments/Addtwonumbers.cs
@@ -1,1 +1,18 @@
+namespace Basic___Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddAndMultiply(2, 4, 5));
+        }
 
+        // | Add two numbers
+        static int AddAndMultiply(int a, int b, int c)
+        {
+            int output = 0;
+            output = (a + b) * c;
+            return output;
+        }
+    }
+}
