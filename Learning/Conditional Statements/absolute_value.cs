58da0cd5aae41afa7f6faebbb7a05fdcea9ebe94
Gabriel Ganderup <146817067+w1setown@users.noreply.github.com>
2024-08-06 11:27:33 +0200

    Update absolute_value.cs

--- Learning/Conditional Statements/absolute_value.cs
+++ Learning/Conditional Statements/absolute_value.cs
@@ -6,7 +6,20 @@ namespace Absolute_value
         {
             Console.WriteLine("Enter an integer:");
             int number = int.Parse(Console.ReadLine());
-            int absoluteValue = Math.Abs(number);
+
+            int absoluteValue = 0;
+            if (number < 0)
+            {
+                if (number != 0)
+                {
+                    absoluteValue = -number;
+                }
+            }
+            else
+            {
+                absoluteValue = number;
+            }
+
             Console.WriteLine($"The absolute value is {absoluteValue}");
         }
     }
